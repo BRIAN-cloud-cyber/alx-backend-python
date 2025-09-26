e91af93710354313a54b4a2d790cdd06ff9f5c55
BRIAN <omondiomimo@gmail.com>
2025-09-26 21:10:09 +0300

    updated

--- messaging_app/messaging_app/chats/permissions.py
+++ messaging_app/messaging_app/chats/permissions.py
@@ -1,22 +1,21 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission,IsAuthenticated
 
-
-class IsMessageOwner(BasePermission):
+class IsParticipantOfConversation(BasePermission):
     """
-    custom permission:only sender or receiver can view/edit a message 
+    - custom permission
+    - only allow autenticated users
+    -only allow participants of a conversation to send/view/update/delete messages
     """
 
-    def has_object_permission(self, request, view, obj):
-        return super().has_object_permission(request, view, obj)
+    def has_permission(self, request, view):
+        return super().has_permission(request, view)
     
+    def has_object_permission(self, request, view, obj):
 
+        if hasattr(obj,"participants"):
 
-    #conversation permission
-
-    class IsConversationParticipant(BasePermission):
-        """
-        custom permission:only participants can access the conversion.
-        """
-        def has_object_permission(self, request, view, obj):
-            return super().has_object_permission(request, view, obj)
+            return request.user in obj.participants.all
+        if hasattr(obj,"sender") and hasattr(obj,"receiver"):
+            return obj.sender ==request.user or obj.receiver ==request.user
         
+        return False 
