dd20dbf4b183f43eb3fe0dc53d0240812b7bdd55
BRIAN <omondiomimo@gmail.com>
2025-10-05 19:25:40 +0300

    signals update

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-
-# Create your models here.
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver  # allows listening of instances
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
@@ -22,3 +21,20 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} about message {self.message.id}"
+    
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for Message ID {self.message.id} edited on {self.edited_at}"
+    
+@receiver(pre_save,sender=Message)
+
+def log_message_edit(sender,instance,**kwargs):
+    if instance.pk:
+        old_message=Message.objects.get(pk=instance.pk)
+        if old_message.content !=instance.object:
+            MessageHistory.objects.create(Message=instance,old_content=old_message.content)
+            instance.edited=True
